5e0bccbf774713ed19b1253dc60a6d507f9e5862
qyang <qyang1219@gmail.com>
2020-02-20 15:21:15 +0800

    添加总结功能，实现了动画

--- pages/message/components/header/header.js
+++ pages/message/components/header/header.js
@@ -1,3 +1,4 @@
+
 // pages/message/components/header/header.js
 Component({
   /**
@@ -11,14 +12,35 @@ Component({
    * 组件的初始数据
    */
   data: {
-
+    showText: false,
+    animation:'',
+    todaySum: 0,
+    weekSum: 0,
+    allSum: 0,
+    maxDay: '2020/2/2',
+    maxDaySum: 0,
+    maxDarkTime:'2020/2/2 12:12:12'
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
-    configIconClick: function(){
+    onReturn: function(){
+      this.opacityAnimation('#center', 1, 0, ()=>{
+        this.setData({
+          showText: false
+        })
+        this.animate('#main',[
+          { height: '100%'},
+          { height: 0 }
+        ], 500, ()=>{
+          this.clearAnimation("#center")
+          this.clearAnimation("#main")
+        })
+      })
+    },
+    onModalShow: function(){
       const data = wx.getStorageInfoSync();
       const {currentSize, limitSize} = data;
       const that = this;
@@ -35,6 +57,29 @@ Component({
           }
         }
       })
+    },
+    configIconClick: function(){
+      this.animate('#main',[
+        { height: 0},
+        { height: '100%'}
+      ], 500, ()=> {
+        this.setData({
+          showText: true
+        })
+        this.opacityAnimation('#center', 0, 1)
+      })
+    },
+    opacityAnimation: function(id, start, end, callback){
+      this.animate(id, [
+        {opacity: start},
+        {opacity: end}
+      ], 500, callback && callback())
+    },
+    heightAnimation: function(id, start, end, callback){
+      this.animate(id, [
+        {height: `${start}%`},
+        {height: `${end}%`}
+      ], 500, callback && callback())
     }
   }
 })
